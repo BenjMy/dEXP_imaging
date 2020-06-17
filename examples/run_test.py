9e212d3f61f5ce46fe969a77644acb649afb283e
BenjiM <benjaminmary.etu@gmail.com>
2020-06-17 22:35:04 +0200

    Update run_test.py

--- examples/run_test.py
+++ examples/run_test.py
@@ -28,18 +28,17 @@ from fatiando.gravmag import prism, imaging, transform
 from fatiando.vis.mpl import square
 
 # my own functions
-import dEXP as dEXP
-from dEXP import _fit
-import plot_dEXP as pEXP
-import set_parameters as para
+# import dEXP as dEXP
+# from dEXP import _fit
+# import plot_dEXP as pEXP
+# import set_parameters as para
 
 # exemples
-import examples.sources_mag.fwd_mag.fwd_mag_sphere as magfwd
-
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['font.size'] = 15
 
+# Create a model using geometric objects from fatiando.mesher
 plt.figure()
 plt.imshow(np.random.random((50,50)))
 plt.colorbar()
