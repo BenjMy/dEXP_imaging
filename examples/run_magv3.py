b2b7830b575e787c2db42964ebff449bf48b957f
BenjiM <benjaminmary.etu@gmail.com>
2020-06-18 10:21:04 +0200

    test run mag doc

--- examples/run_magv3.py
+++ examples/run_magv3.py
@@ -32,10 +32,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['font.size'] = 15
 
-
-# simulate mag data 
-xp, yp, zp, U, shape, p1, p2, coord= magfwd.load_mag_synthetic()
-
 # Create a model using geometric objects from fatiando.mesher
 plt.figure()
 plt.imshow(np.random.random((50,50)))
