b0f32bfd7fddec9e99d842fdc6b8828cfcf88cd2
BenjiM <benjaminmary.etu@gmail.com>
2020-06-22 12:31:10 +0200

    Update run_mag_test.py

--- examples/run_mag_test.py
+++ examples/run_mag_test.py
@@ -16,11 +16,6 @@ import examples.sources_mag.fwd_mag_sphere as magfwd
 
 
 # Create a model using geometric objects from fatiando.mesher
-plt.figure()
-plt.imshow(np.random.random((50,50)))
-plt.colorbar()
-plt.show()
-
 xp, yp, zp, U, shape, p1, p2, coord= magfwd.load_mag_synthetic()
 max_elevation=2*max(coord[:,2])
 scaled, SI, zp, qorder, nlay, minAlt_ridge, maxAlt_ridge = para.set_par(shape=shape,max_elevation=max_elevation)
@@ -28,3 +23,21 @@ interp = True
 
 # Plot field data over a 2d line
 pEXP.plot_line(xp, yp, U,p1,p2, interp=interp)
+
+# Upward continuation of the field data
+mesh, label_prop = dEXP.upwc(xp, yp, zp, U, shape, 
+                 zmin=0, zmax=max_elevation, nlayers=nlay, 
+                 qorder=qorder)
+
+plt, cmap = pEXP.plot_xy(mesh, label=label_prop)
+plt.colorbar(cmap)
+
+# Ridges identification
+dEXP.ridges_minmax_plot(xp, yp, mesh, p1, p2,
+                                      label=label_prop,
+                                      method_peak='find_peaks')  
+
+# or find_peaks or peakdet or spline_roots
+dfI,dfII, dfIII = dEXP.ridges_minmax(xp, yp, mesh, p1, p2,
+                                      label=label_prop,
+                                      method_peak='find_peaks')  
