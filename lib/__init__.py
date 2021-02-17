c32c6c057dc3f4de68d90732e08df639098f354e
BenjiM <benjaminmary.etu@gmail.com>
2021-02-17 13:29:05 +0100

    new init

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,17 +1,8 @@
 """
 dEXP for potential field source depth estimation
-
-dEXP implementation
---------------------
-* :mod:`~dEXP_imaging.dEXP`
-
-dEXP plotter
---------------------
-* :mod:`~dEXP_imaging.plot_dEXP`
-
-dEXP utils
---------------------
-* :mod:`~dEXP_imaging.utils_dEXP`
-----
-
 """
+
+import dEXP
+import plot_dEXP
+import utils_dEXP
+import set_parameters
